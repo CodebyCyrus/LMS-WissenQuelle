d760ab332a4304e974bbfb59c98c2a432e65d6a7
PersianDavinci <persiandavinci@gmail.com>
2024-05-10 15:39:37 +0330

    add some setting to backend-setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Custom Apps
+    'core',
+    'userauths',
+    'api',
 ]
 
 MIDDLEWARE = [

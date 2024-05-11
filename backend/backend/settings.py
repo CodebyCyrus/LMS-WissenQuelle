62b95d55166f0466efcd78eb8158dd2bf49f08c0
PersianDavinci <persiandavinci@gmail.com>
2024-05-11 15:42:06 +0330

    add jazzmin theme config in setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -127,3 +127,33 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "WissenQuelle LMS Admin",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "WissenQuelle",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "WissenQuelle",
+
+    # Logo to use for your site, must be present in static files, used for brand on top left
+    # "site_logo": "static/img/logo.png",
+
+#   # Logo to use for your site, must be present in static files, used for login form logo (defaults to site_logo)
+#     "login_logo": None,
+
+#     # Logo to use for login form in dark themes (defaults to login_logo)
+#     "login_logo_dark": None,
+#   # Relative path to a favicon for your site, will default to site_logo if absent (ideally 32x32 px)
+#     "site_icon": None,
+
+#     # Welcome text on the login screen
+#     "welcome_sign": "Welcome to the library",
+
+#     # Copyright on the footer
+    "copyright": "WissenQuelle by Mohammad Makaremnejad - codebycyrus.com",
+
+}

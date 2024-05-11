7a6de9aaacc05b383b0cd71e63c790b842c9404f
PersianDavinci <persiandavinci@gmail.com>
2024-05-11 15:53:23 +0330

    add jazzmin Theme basics in seeting and show_ui_builder : False,

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -157,8 +157,40 @@ JAZZMIN_SETTINGS = {
 #     # Copyright on the footer
     "copyright": "WissenQuelle by Mohammad Makaremnejad - codebycyrus.com",
 
-"show_ui_builder" : True,
+"show_ui_builder" : False,
 
 
 
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-purple navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "default",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
 }

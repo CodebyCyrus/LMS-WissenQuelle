4b226a30a5bf2159b0f4eec75a4b786101140df2
PersianDavinci <persiandavinci@gmail.com>
2024-05-11 16:17:55 +0330

    fix an error about static folder

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -124,13 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
-
 STATIC_ROOT = BASE_DIR / 'templates'
 
 MEDIA_URL = '/media/' # 127.0.0.1/media/avatar.jpg
-
 MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type

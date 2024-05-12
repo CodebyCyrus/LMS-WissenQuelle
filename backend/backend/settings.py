d71e4ef04d243240ffacf6e417706f7f0a5f8507
PersianDavinci <persiandavinci@gmail.com>
2024-05-12 17:10:45 +0330

    migiration and migrate files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,8 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # 'jazzmin',
-    # 'django.contrib.admin',
+    'jazzmin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',

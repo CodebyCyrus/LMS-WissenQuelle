15d8875c4326ef8623109bef4f37cdcf42f8d89c
PersianDavinci <persiandavinci@gmail.com>
2024-05-12 17:55:27 +0330

    add django signal setup for automatic user create and save data

--- backend/userauths/models.py
+++ backend/userauths/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
-
+from django.db.models.signals import post_save
 
 class User(AbstractUser):
     username = models.CharField(unique=True, max_length=100)
@@ -45,3 +44,14 @@ def save(self, *args, **kwargs):
         self.full_name == self.user.username
         
     super(Profile, self).save("*args, **kwargs")
+
+
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    post_save.connect(create_user_profile, sender=User)
+    post_save.connect(save_user_profile, sender=User)

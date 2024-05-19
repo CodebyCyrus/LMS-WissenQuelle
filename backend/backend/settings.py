b176b51a92bc12e8da4ea2129bb35a168f754841
PersianDavinci <persiandavinci@gmail.com>
2024-05-19 15:24:59 +0330

    Install on setting.py # Add CORSHeader Below 'corsheaders.middleware.CorsMiddleware',

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -56,6 +56,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

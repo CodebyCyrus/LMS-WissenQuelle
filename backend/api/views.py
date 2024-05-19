1d98a3705fc00268b30558dd53acaad159de5726
PersianDavinci <persiandavinci@gmail.com>
2024-05-19 16:54:19 +0330

    views.py api  part 1

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from api import serializer as api_serializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = api_serializer.MyTokenObtainPairSerializer

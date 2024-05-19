dd6aa0f27fde03263fbee44b729dbba46df74af4
PersianDavinci <persiandavinci@gmail.com>
2024-05-19 16:39:53 +0330

    serializer.py Login part 1

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -1,6 +1,19 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
 from userauths.models import Profile, User
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+     @classmethod
+     def get_token(cls, user):
+          token = super().get_token(user)
+          
+          token['full_name'] = user.full_name
+          token['email'] = user.email
+          token['username'] = user.username
+          
+          return token
+          
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
